422d424581774f5df76eef6bc066c312c6e04648
Demid65 <Demid65e@yandex.ru>
2020-01-16 19:26:45 +0300

    Revert "Revert "Initial Commit""
    This reverts commit dca0b67141b498873d1bfab8bcfd77f3baaf30ea.

---
+++ Autonet/Arduino.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Autonet
+{
+    class Arduino
+    {/*
+        public Arduino(String Port)
+        {
+            SPort.PortName = Port;
+            SPort.BaudRate = 9600;
+            SPort.Open();
+            Thread.Sleep(100);
+
+        }
+        */
+        protected SerialPort SPort = new SerialPort();
+        
+        public void Init(String Port)
+        {
+            SPort.PortName = Port;
+            SPort.BaudRate = 9600;
+            SPort.Open();
+            Thread.Sleep(100);
+
+        }
+
+        public void DigitalWrite(int Pin, int Level)
+        {
+            SPort.WriteLine(String.Format("({0})[{1}]<DigitalWrite>", Level.ToString(), Pin.ToString()));
+            Thread.Sleep(10);
+        }
+
+        public void AnalogWrite(int Pin, int Level)
+        {
+            SPort.WriteLine(String.Format("({0})[{1}]<AnalogWrite>", Level.ToString(), Pin.ToString()));
+            Thread.Sleep(10);
+        }
+
+        public void PinMode(int Pin, int Mode)
+        {
+            SPort.WriteLine(String.Format("({0})[{1}]<PinMode>", Mode.ToString(), Pin.ToString()));
+            Thread.Sleep(10);
+        }
+
+        public int DigitalRead(int Pin)
+        {
+            SPort.WriteLine(String.Format("[{0}]<DigitalRead>", Pin.ToString()));
+            Thread.Sleep(10);
+            return int.Parse(SPort.ReadLine());
+        }
+
+        public int AnalogRead(int Pin)
+        {
+            SPort.WriteLine(String.Format("[{0}]<AnalogRead>", Pin.ToString()));
+            Thread.Sleep(10);
+            return int.Parse(SPort.ReadLine());
+        }
+    }
+}
